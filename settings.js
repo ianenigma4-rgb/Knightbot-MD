2db4c4de7eb71e72e696548ed0090f6f107de9a3
ianenigma4-rgb <ianenigma4@gmail.com>
2026-04-27 21:22:54 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "IANENIGMA MD",
   botOwner: 'THEIANENIGMACEO', // Your name
-  ownerNumber: '256775063417', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '256746724547', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
